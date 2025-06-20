0ad8103ab557cad9cac16e1996ffc4b46a243fd7
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-20 21:01:39 +0700

    Simulasi Blog Posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('container')
+    <article>
+        <h2>Judul</h2>
+        <h5>By: Author</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+
+    </article>
+        <a href="/blog" class="d-block mt-3">Kembali ke daftar blog</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,44 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home',
-        [
-            "title" => "home",
-        ]
-    );
+    return view('home', [
+        "title" => "Home",
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "about",
+        "title" => "About",
         "name" => "Abdi Putra ZUlkarnain",
         "email"=> "mhdabdikc123@gmail.com",
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts',[
-        'title'=> 'posts',
+$blog_posts = [
+    [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Abdi Putra Zulkarnain",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque."
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Abdi Putra Zulkarnain",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque."
+    ]
+    ];
+
+    return view('posts', [
+        'title' => "Posts",
+        'posts' => $blog_posts
+    ]);
+});
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    return view('post', [
+        'title'=> 'Single Post',
     ]);
 });
