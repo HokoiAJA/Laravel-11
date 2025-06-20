d60ddf72a1c14ceee688e115697a9ed238906f3b
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-20 20:26:53 +0700

    Layouting System

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',
+        [
+            "title" => "home",
+        ]
+    );
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "about",
         "name" => "Abdi Putra ZUlkarnain",
         "email"=> "mhdabdikc123@gmail.com",
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts',[
+        'title'=> 'posts',
+    ]);
 });
