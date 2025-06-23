71fda6f134bb7a0968450c349ac00eac01fa0b60
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-23 19:51:24 +0700

    Mass Assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    
+    use HasFactory;
+
+    // protected $fillable =['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
