a5adcdcaf2cdc2db3dc10c64227add9fd8b92ca9
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-24 00:03:37 +0700

    Membuat seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Abdi Putra Zulkarnain',
+            'email' => 'mhdabdikc123@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Ini adalah cuplikan dari judul pertama',
+            'body' => 'Ini adalah isi lengkap dari judul pertama. Ini adalah contoh teks yang digunakan untuk mengisi konten dari judul pertama.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Ini adalah cuplikan dari judul kedua',
+            'body' => 'Ini adalah isi lengkap dari judul kedua. Ini adalah contoh teks yang digunakan untuk mengisi konten dari judul kedua.',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
+    }
+}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <p>By. Sandhika Galih in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+            <p>By. <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
             </p>
             <p>{{ $post->excerpt }}</p>
             <a href="/posts/{{ $post->slug }}">Read more..</a>
