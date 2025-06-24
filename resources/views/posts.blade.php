0785b4625c48139e346b605e729a0a46722f2cfd
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-24 12:30:12 +0700

    Menambahkan post image (unsplash api)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,19 @@
     <h1 class="mb-5">{{ $title }}</h1>
     @if ($posts->count())
         <div class="card mb-3">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>"
-                <p class="card-text"><smail class="text-muted">Last updated 3 mins ago</small></p>"
+            <img src="https://picsum.photos/1200/400?random={{ $posts[0]->category->name }}" alt="...">
+            <div class="card-body text-center">
+                <h3 class="card-title"><a href="/$posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title}}</a></h3>
+                <p>
+                <small class="text-muted"> 
+                By. <a href="/authors/{{ $posts[0]->author->username }}" 
+                class="text-decoration-none">{{ $posts[0]->author->name }}
+                </a> in <a href="/categories/{{ $posts[0]->category->slug }}" 
+                class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                </small></p>
+                <p class="card-text">{{ $posts[0]->excerpt }}</p>
+
+                <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
             </div>
         </div>
     @else
