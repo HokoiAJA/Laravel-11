26f4e95e4722cd1a6c04af7200d0ff1fd2930adb
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-24 22:21:38 +0700

    Insert data user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -15,12 +18,12 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        $request->validate([
+        $validateData = $request->validate([
             'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users',
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
-        dd('Berhasil mendaftar!');
+        User::create($validateData);
     }
 }
