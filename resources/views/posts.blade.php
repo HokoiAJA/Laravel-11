e1115d0cdf38939d8ad9a66ac93a771fde0ac3c7
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-24 18:52:14 +0700

    Query filter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,6 +5,12 @@
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Search.." name="search"
                         value="{{ request('search') }}">
@@ -23,9 +29,9 @@
                             class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
                     <p>
                         <small class="text-muted">
-                            By. <a href="/authors/{{ $posts[0]->author->username }}"
+                            By. <a href="/posts?author={{ $posts[0]->author->username }}"
                                 class="text-decoration-none">{{ $posts[0]->author->name }}</a> in
-                            <a href="/categories/{{ $posts[0]->category->slug }}"
+                            <a href="/posts?category={{ $posts[0]->category->slug }}"
                                 class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                             {{ $posts[0]->created_at->diffForHumans() }}
                         </small>
@@ -41,7 +47,7 @@
                         <div class="col-md-4">
                             <div class="card">
                                 <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a
-                                        href="/categories/{{ $post->category->slug }}"
+                                        href="/posts?category={{ $post->category->slug }}"
                                         class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                                 <img src="https://picsum.photos/500/400?random={{ $post->category->name }}"
                                     class="card-img-top" alt="{{ $post->category->name }}">
@@ -49,7 +55,7 @@
                                     <h5 class="card-title">{{ $post->title }}</h5>
                                     <p>
                                         <small class="text-muted">
-                                            By. <a href="/authors/{{ $post->author->username }}"
+                                            By. <a href="/posts?author={{ $post->author->username }}"
                                                 class="text-decoration-none">{{ $post->author->name }}</a> in <a
                                                 href="/categories/{{ $post->category->slug }}"
                                                 class="text-decoration-none">{{ $post->category->name }}</a>
