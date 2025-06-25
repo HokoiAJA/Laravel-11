246f00b0c6db186539fbafe19d2cb7309511ca20
HokoiAJA <mhdabdikc123@gmail.com>
2025-06-25 21:09:00 +0700

    Membuat combo box Category

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
@@ -26,7 +27,7 @@ class DashboardPostController extends Controller
     public function create()
     {
         return view('dashboard.posts.create', [
-            'post' => new Post()
+            'categories' => Category::all()
         ]);
     }
 
