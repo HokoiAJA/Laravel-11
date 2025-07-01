571d7e43688ed76b2871ed97a8eb7c39b6a51935
HokoiAJA <mhdabdikc123@gmail.com>
2025-07-01 09:05:13 +0700

    Memperbaiki validasi slug

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -78,7 +78,15 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required',
+        ];
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+        $validatedData = $request->validate($rules);
     }
 
     /**
